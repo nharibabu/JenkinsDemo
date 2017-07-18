81b0c5423565de43664f85458d2035c3b32363d3
JMathews4 <JMathews4@CSCINDAI423753>
2017-07-18 16:00:39 +0530

    Added sample test project

--- JenkinsDemo/Controllers/HomeController.cs
+++ JenkinsDemo/Controllers/HomeController.cs
@@ -14,5 +14,15 @@ namespace JenkinsDemo.Controllers
 
             return View();
         }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
---
+++ JenkinsDemoTest/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using JenkinsDemo.Controllers;
+
+namespace JenkinsDemoTest
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void AddTest()
+        {
+            HomeController ctrl = new HomeController();
+            int sum = ctrl.Add(1, 2);
+            Assert.AreEqual(3, sum);
+        }
+
+        [Test]
+        public void SubtractTest()
+        {
+            HomeController ctrl = new HomeController();
+            int sub = ctrl.Subtract(2, 1);
+            Assert.AreEqual(1, sub);
+        }
+    }
+}
