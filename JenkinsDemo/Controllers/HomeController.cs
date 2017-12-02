081ad99eaaada3244e5e61c8da3c501a39b87f4b
Mathews, Jaishs <jmathews4@csc.com>
2017-12-02 23:59:37 +0530

    Added title

--- JenkinsDemo/Controllers/HomeController.cs
+++ JenkinsDemo/Controllers/HomeController.cs
@@ -1,28 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace JenkinsDemo.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Home Page";
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			string s = Server.MapPath("~");
+			DirectoryInfo info = new DirectoryInfo(s);
+			ViewBag.Title = string.Format("Home Page - {0} - {1}", info.LastWriteTimeUtc.ToLongDateString(), info.LastWriteTimeUtc.ToLongTimeString());
+			return View();
+		}
 
-            return View();
-        }
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
 
-        public int Add(int a, int b)
-        {
-            return a + b;
-        }
-
-        public int Subtract(int a, int b)
-        {
-            return a - b;
-        }
-    }
+		public int Subtract(int a, int b)
+		{
+			return a - b;
+		}
+	}
 }
